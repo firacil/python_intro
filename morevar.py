8ee3126a2bd0c20d1cae0cda232f3cd09b8277f2
firacil <firaolmekbib511@gmail.com>
2023-12-20 23:44:45 -0800

    more on variables

---
+++ morevar.py
@@ -0,0 +1,64 @@
+#as i said python hs no command for declaring variable.
+# variable will be created at the moment we first assign a value to it.
+
+#1 and now let's try additional thing on variable.
+
+x = 5
+x = "I am Not Five"
+
+# what is the out put ??
+#so, python can change type after they have been set.
+
+print(x)
+
+#2 Casting
+#If i want to specify the data type of variable, this can be done with casting.
+
+i = int(8)
+j = float(8)
+k = str(8)
+
+print (i)
+print (j)
+print (k)
+
+#try the output before excuting
+
+#3 getting type of variable.
+# to get type of variable we will use type() function.
+
+l = 5
+m = "Firaol"
+
+print(type(l))
+print (type(m))
+
+#try the output before excution
+
+#4 single and double quotes
+# string variables can be declared using either single or double quotes
+
+n = "Firaol"
+# same as
+o = 'Firaol'
+
+#try to writedown outputs before excution
+
+print(n)
+print(o)
+
+#5 Case-Sensitive
+#variable names are case sensitive.
+
+a = 90
+A = 'ninty'
+
+#this two variables is not the same a and A
+#try outputs before excution
+
+print(a)
+print (A)
+
+"""
+Well Done You are Doing Good.
+"""
