336a51ff7a4473e70e7401069d333510863a7259
firacil <firaolmekbib511@gmail.com>
2023-12-20 23:14:48 -0800

    variables and comments

---
+++ variable.py
@@ -0,0 +1,17 @@
+#in python variable will created when assigning value.
+#let me give you example below.
+
+x = 10
+y = "I am"
+
+print (y, x)
+
+#this will print I am 10
+#so python has no command for declaring a variable just assign value.
+#comment starts with '#' symbol.
+
+"""
+this can be 
+multi line
+comment
+"""
