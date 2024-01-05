41a66ebc7f4c91117dbd75898c24ea43ad2849aa
firacil <firaolmekbib511@gmail.com>
2024-01-05 02:03:05 -0800

    updated

---
+++ lists.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''list data structure in python is the most versatile and can be written
+as a list of comma-separated values(items) between square brakets'''
+
+square = [1, 2, 3, 4, 5]
+print(square[0])
+print(square[-1])
