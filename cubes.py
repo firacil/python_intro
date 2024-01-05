475648ef8e8931d06ce32704598da430bffc957e
firacil <firaolmekbib511@gmail.com>
2024-01-05 03:03:46 -0800

    updated

--- cubes.py
+++ cubes.py
@@ -4,3 +4,12 @@ cubes = [1, 8, 27, 65, 125]
 
 cubes[3] = 64 # replaced
 print(cubes)
+''' let's append a value to cubes
+    we will do for bothe cube of 6 and 7'''
+
+cubes.append(6 ** 3)
+cubes.append(7 ** 3)
+
+# let's print after appending value
+
+print(cubes)
